c8e01b256b268502e9354756c1802a3957136fa7
edward-Han1024 <babyed.han@gmail.com>
2024-08-02 10:32:52 -0700

    VERSION V0.0.1 a lot of new things! see CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+## Notes
+This document continains the changes.
+It was added in Version v0.0.2 (scroll to the bottom if needed).
+I contains the most recent first.
+The log is seperated into two parts every log:
+the main part and
+what happened in the Github repo that did not affect the page itself.
+## Log
+### VERSION V0.0.2
+ - Added visibility and traveling. 
+ - Added a turn count. 
+ - Added some helping functions. 
+ - Fixed some functions
+#### Github Actions
+ - Made Repository Public
+ - Started Pages
+ - added .gitignore
+ - added list of functions for debugging
+### VERSION V0.0.1
+ - Made the Game!
+ - Fixed the html for this specific project
+#### Github actions
+ - Created this Repo
+ - Uploaded Files
--- main.mjs
+++ main.mjs
@@ -22,7 +22,13 @@ document.getTableElement = (x,y) => {
 	let res = $(`#table tr:nth-child(${y}) td:nth-child(${x}`);
 	return res
 }
-document.loadMap = (plane) => {
+// v  v  v  v  v // v LOADED DATA v // ████████████████████████████████████████████████████████████████████████████████████
+document.Game = JSON.parse(JSON.stringify(Data));
+document.Game.Turn = (turns) => {
+	document.Game.Nturn += turns;
+	document.Game.Update();
+}
+document.Game.LoadMap = (plane) => {
 	let a = 0
 	let b = 0
 	if (plane == 0){a = 0;b = 0;}
@@ -32,17 +38,98 @@ document.loadMap = (plane) => {
 	for (let i = 1; i <= 16; i++){
 		for (let j = 1; j <= 16; j++){
 			if (document.Game.Visibility[a][b][j-1][i-1]){
-				let element = document.getTableElement(i,j)
-				let value = document.Game.Map[a][b][j-1][i-1]
-				element.html(value);
+				let element = document.getTableElement(i,j);
+				let value = document.Game.Map[a][b][j-1][i-1];
+				element.html(document.Game.Values[value].Name);
 				element.css({
 					"background-color": document.Game.Values[value].Color,
 					"color": document.Game.Values[value].TextColor
-				})
+				});
 			}
 		}
 	}
 }
-// v  v  v  v  v // v LOADED DATA v // ████████████████████████████████████████████████████████████████████████████████████
-document.Game = JSON.parse(JSON.stringify(Data));
-document.loadMap(0);
+// Plane Actions
+document.Game.GetPlane = (x = 0, y = 0) => {
+	if (!x){
+		let x = document.Game.Position[0]
+	}
+	if (!y){
+		let y = document.Game.Position[1]
+	}
+	if (x<=16 && y<=16){return 0}
+	if (x>16 && y<=16){return 1}
+	if (x<=16 && y>16){return 2}
+	if (x>16 && y>16){return 3}
+}
+document.Game.ReducePlane = (plane) => { // Returns (x, y)
+	if (plane==0){return [0,0]}
+	if (plane==1){return [1,0]}
+	if (plane==2){return [0,1]}
+	if (plane==3){return [1,1]}
+}
+document.Game.VisibleLoad = () => {
+	let pos = document.Game.Position;
+	let Map = document.Game.Map;
+	let Values = document.Game.Values
+	function plane(pos){
+		return document.Game.ReducePlane(document.Game.GetPlane(pos[0],pos[1]))
+	}
+	// pos[0]+1, pos[1]:
+	let newpos = [pos[0]+1, pos[1]]
+	let planet = plane(newpos)
+	let Value = Map[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16]
+	if (Values[Value].Visibility >= document.Game.VPower){
+		document.Game.Visibility[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16] = true
+	}
+	// pos[0], pos[1]+1:
+	newpos = [pos[0], pos[1]+1]
+	planet = plane(newpos)
+	Value = Map[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16]
+	if (Values[Value].Visibility >= document.Game.VPower){
+		document.Game.Visibility[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16] = true
+	}
+	// pos[0]-1, pos[1]:
+	newpos = [pos[0]-1, pos[1]]
+	planet = plane(newpos)
+	Value = Map[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16]
+	if (Values[Value].Visibility >= document.Game.VPower){
+		document.Game.Visibility[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16] = true
+	}
+	// pos[0], pos[1]-1:
+	newpos = [pos[0], pos[1]-1]
+	planet = plane(newpos)
+	Value = Map[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16]
+	if (Values[Value].Visibility >= document.Game.VPower){
+		document.Game.Visibility[planet[1]][planet[0]][(newpos[1] - 1) % 16][(newpos[0] - 1) % 16] = true
+	}
+}
+document.Game.Update = (plane = document.Game.GetPlane()) => {
+	document.Game.VisibleLoad()
+	document.Game.LoadMap(plane);
+	$("#turn").html(document.Game.Nturn);
+}
+document.Game.Move = (dir) => {
+	let pos = (0,0);
+	if (dir == 0){
+		pos = [document.Game.Position[0] + 1, document.Game.Position[1]];
+	}
+	if (dir == 1){
+		pos = [document.Game.Position[0], document.Game.Position[1] - 1];
+	}
+	if (dir == 2){
+		pos = [document.Game.Position[0] - 1, document.Game.Position[1]];
+	}
+	if (dir == 3){
+		pos = [document.Game.Position[0], document.Game.Position[1] + 1];
+	}
+	if (1<=pos[0]<=32 && 1<=pos[1]<=32){
+		let plane = document.Game.ReducePlane(document.Game.GetPlane(pos[0],pos[1]));
+		let value = document.Game.Map[plane[1]][plane[0]][(pos[1]-1) % 16][(pos[0]-1) % 16]
+		if (document.Game.Values[value].Travel <= document.Game.TPower){
+			document.Game.Turn(document.Game.Values[value].Travel);
+			document.Game.Position = pos;
+		}
+	}
+}
+document.Game.Update()
